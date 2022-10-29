eef035043f9ab2d3e11c61a38301f768ca462379
Douglas Francisco Cardoso <douglasfrancardoso@gmail.com>
2022-10-29 19:05:16 -0300

    Created the basic infrastructure

---
+++ iac/artifactregistry.tf
@@ -0,0 +1,9 @@
+resource "google_project_service" "artifactregistry" {
+  service = "artifactregistry.googleapis.com"
+}
+
+resource "google_artifact_registry_repository" "my-repo" {
+  location      = "us-central1"
+  repository_id = "openfoodfacts-datasets"
+  format        = "DOCKER"
+}
--- iac/main.tf
+++ iac/main.tf
@@ -1,18 +1,34 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "4.41.0"
-    }
-  }
-}
-
 provider "google" {
-  # Configuration options
   project = "openfoodfacts-datasets"
 }
 
-resource "google_project_service" "project" {
-    project = "openfoodfacts-datasets"
-    service = "workflows.googleapis.com"
+resource "google_project_service" "workflows" {
+  service = "workflows.googleapis.com"
+}
+
+resource "google_service_account" "workflow" {
+  account_id   = "workflow"
+  display_name = "Workflow Service Account"
+}
+
+resource "google_storage_bucket" "openfoodfacts-datasets" {
+  name          = "openfoodfacts-datasets"
+  location      = "US"
+  force_destroy = true
+}
+
+resource "google_storage_bucket_acl" "image-store-acl" {
+  bucket = google_storage_bucket.openfoodfacts-datasets.name
+
+  role_entity = [
+    "OWNER:user-${google_service_account.workflow.email}",
+  ]
+}
+
+resource "google_workflows_workflow" "workflow" {
+  name            = "workflow"
+  region          = "us-central1"
+  description     = "Magic"
+  service_account = google_service_account.workflow.id
+  source_contents = file("${path.module}/../workflow.yaml")
 }
