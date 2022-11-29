2b4e26cfb2c2bdf848b8d260d5c44cb0b62b9693
Douglas Francisco Cardoso <douglasfrancardoso@gmail.com>
2022-11-29 20:22:08 -0300

    Create another workflow

--- terraform/iac/workflows.tf
+++ terraform/iac/workflows.tf
@@ -11,7 +11,7 @@ resource "google_workflows_workflow" "import-openfoodfacts-products-to-bigquery"
   region          = "us-central1"
   description     = "ETL To import jsonl to bigquery table"
   service_account = google_service_account.workflow.id
-  source_contents = file("${path.module}/../../jobs/import-openfoodfacts-products-to-bigquery/workflow.yaml")
+  source_contents = file("${path.module}/../../jobs/import-openfoodfacts-products-to-bigquery/workflow.yml")
 }
 
 resource "google_cloud_scheduler_job" "import-openfoodfacts-products-to-bigquery" {
@@ -25,6 +25,33 @@ resource "google_cloud_scheduler_job" "import-openfoodfacts-products-to-bigquery
   http_target {
     http_method = "POST"
     uri         = "https://workflowexecutions.googleapis.com/v1/projects/${var.project}/locations/${var.location}/workflows/${google_workflows_workflow.import-openfoodfacts-products-to-bigquery.name}/executions"
+    body        = base64encode("{\"argument\":\"{}\",\"callLogLevel\":\"LOG_ALL_CALLS\"}")
+
+    oauth_token {
+      service_account_email = google_service_account.workflow.email
+    }
+  }
+}
+
+resource "google_workflows_workflow" "execute-dataform" {
+  name            = "execute-dataform"
+  region          = "us-central1"
+  description     = "ETL To create the trusted layer"
+  service_account = google_service_account.workflow.id
+  source_contents = file("${path.module}/../../jobs/execute-dataform/workflow.yml")
+}
+
+resource "google_cloud_scheduler_job" "execute-dataform" {
+  name             = "execute-dataform"
+  description      = "execute-dataform"
+  schedule         = "0 8  * * 7"
+  time_zone        = "America/Sao_Paulo"
+  attempt_deadline = "320s"
+  region           = var.location
+
+  http_target {
+    http_method = "POST"
+    uri         = "https://workflowexecutions.googleapis.com/v1/projects/${var.project}/locations/${var.location}/workflows/${google_workflows_workflow.execute-dataform.name}/executions"
     body        = base64encode("{\"argument\":\"{}\",\"callLogLevel\":\"LOG_ALL_CALLS\"}")
 
     oauth_token {
